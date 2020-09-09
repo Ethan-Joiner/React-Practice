34570e9490a85f8805b46b4c79e3dca3677f3b7d
Ethan <Ethan@Ethans-MBP.satx.rr.com>
2020-09-09 11:27:04 -0500

    added interactivity

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return ( <
-            button className = "square" > { this.props.value } <
+            button className = "square"
+            onClick = {
+                () => alert('click')
+            } >
+            { this.props.value } <
             /button>
         );
     }
